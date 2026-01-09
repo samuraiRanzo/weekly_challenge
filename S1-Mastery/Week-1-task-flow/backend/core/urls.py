e88b31245c8d3da41d1730311b90e4f1e5bb925d
samuraiRanzo <ranzo.samurai@gmal.com>
2026-01-09 12:07:19 +0800

    test ci/cd workflow

---
+++ S1-Mastery/Week-1-task-flow/backend/core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('auth/register/', views.RegisterView.as_view(), name='auth-register'),
+    path('auth/login/', views.EmailLoginView.as_view(), name='auth-login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='auth-refresh'),
+    path('auth/user/', views.UserView.as_view(), name='auth-user'),
+    path('hello/', views.hello, name='hello'),
+]
